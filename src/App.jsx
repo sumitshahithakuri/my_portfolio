b9803f0c7e7a3e80e7d3d8ad3efdb3049dc283f5
LEO <leo@leo.leo>
2025-08-14 09:45:01 +0545

    Tailwind and vite setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  return (
+    <>
+    <div className='w-6xl mx-auto'>
+
+    <p className='text-green-500 font-bold text-center'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod quaerat expedita aperiam, ipsam ullam consequuntur molestiae dolore velit voluptas eveniet sed atque? Asperiores!</p>
+    
+    </div>
+    </>
+   )
+}
+
+export default App
